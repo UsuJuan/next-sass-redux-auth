df167771fa3c4edeca4439d21df5703ef0698d39
Juan Ortiz <juan.ortiz.oviedo@gmail.com>
2019-08-26 08:02:20 -0500

    Change package.json

---
+++ pages/components/navBar.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export default function NavBar () {
+  return (<ul>
+      <li>
+        <a href="#">item 1</a>
+      </li>
+      <li>
+        <a href="#">item 2</a>
+      </li>
+      <li>
+        <a href="#">item 3</a>
+      </li>
+      <li>
+        <a href="#">item 4</a>
+      </li>
+    </ul>);
+}
